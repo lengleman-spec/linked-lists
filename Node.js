51132a3cf9917588601908938b10fbcc088f7984
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-23 13:53:49 -0400

    Set both value and nextNode to null so that we can create nodes easier.

--- Node.js
+++ Node.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value, nextNode) {
+  constructor(value = null, nextNode = null) {
     this.value = value; // store the value
     this.nextNode = nextNode; // link to next node
   }
